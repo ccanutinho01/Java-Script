f9c2f0560bd37fb762959b054e9cf11128c3e592
ccanutinho01 <ccanutinho01@gmail.com>
2025-08-29 21:35:34 -0300

    atualizações de arquivos

--- Eventos/JS/script.js
+++ Eventos/JS/script.js
@@ -14,3 +14,50 @@ function mudarTexto() {
     msg.style.color = "blue";
     msg.style.fontWeight = "bold";
 }
+const texto= document.getElementById("texto");
+
+texto.addEventListener("mouseover",()=>{
+    texto.classList.add("destaque");
+});
+
+texto.addEventListener("mouseout",()=>{
+    texto.classList.remove("destaque");
+});
+
+document.getElementById("enviar").addEventListener("click", ()=>{
+    const nome= document.getElementById("nome").value;
+    document.getElementById("resultado").textContent='Olá, ${nome}!';
+})
+document.getElementById("adicionar").addEventListener("cick",()=>{
+    const novoItem = document.createElement("li");
+    novoItem.textContent= "Item adicionado dinamicamente";
+    document.getElementById("lista").appendChild(novoItem);
+})
+
+function somar(a, b){
+    return a + b;
+}
+
+function calcular(){
+    const h1= parseFloat(document.getElementById("num1").value);
+    const h2= parseFloat(document.getElementById("num2").value);
+    const resultado= somar(h1, h2);
+    document.getElementById("resultado").textContent= "Resultado: " + resultado;
+}
+document.getElementById("enviar").addEventListener("click", ()=>{
+    const msg= document.getElementById("msg");
+    msg.textContent = "Salvando...";
+
+    setTimeout(() => {
+        msg.textContent="Dados salvos com sucesso";
+}, 2000);
+
+});
+fetch('https://jsonplaceholder.typicode.com/posts/1')
+.then(response => response.json())
+.then(data=>{
+    console.log("Dados recebidos: ", data);
+    document.body.innerHTML += 
+    `<h2>${data.tittle}</h2><p>${data.body}</p>`;
+})
+.catch(error => console.error("Erro na requisição", error));
